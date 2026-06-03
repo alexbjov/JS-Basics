9e997bda6b0b63a212a0952f2fe3307a22449360
alexbj <ajoveski@gmail.com>
2026-06-04 00:32:04 +0200

    Add exercises to chapter 06

---
+++ 06-nested-loops/sum-prime-nonprime.js
@@ -0,0 +1,33 @@
+function solve(input) {
+	let primeSum = 0;
+	let nonPrimeSum = 0;
+
+	let index = 0;
+	while (input[index] !== "stop") {
+		const number = Number(input[index]);
+
+		if (number < 0) console.log("Number is negative.");
+		else {
+			let isPrime = true;
+			let numSqrt = Math.sqrt(number);
+			for (let i = 2; i <= numSqrt; i++) {
+				if (number % i === 0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime) primeSum += number;
+			else nonPrimeSum += number;
+		}
+
+		index++;
+	}
+
+	console.log(`Sum of all prime numbers is: ${primeSum}`);
+	console.log(`Sum of all non prime numbers is: ${nonPrimeSum}`);
+}
+
+solve(["3", "9", "0", "7", "19", "4", "stop"]);
+// solve(["30", "83", "33", "-1", "20", "stop"]);
+// solve(["0", "-9", "0", "stop"]);
